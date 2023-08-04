2fbe87dd530d71fb4223b003c75ff094f2b7347a
Thembeka Nguza <Thembeka.Nguza@ekurhuleni.gov.za>
2023-08-04 14:08:38 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ProductlistingComponent } from './productlisting/productlisting.component';
+import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",pathMatch:"full",component:AppComponent
+  },
+
+
+  {
+    path:"main",component:MainContentComponent
+  },
+
+  {
+    path:"Productlisting",component:ProductlistingComponent
+  },
+
+  {
+    path:"ProductDetails",component:ProductDetailsComponent
+  }
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ProductlistingComponent } from './productlisting/productlisting.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HeaderComponent,
+    SearchbarComponent,
+    ContentComponent,
+    FooterComponent,
+    MainContentComponent,
+    ProductlistingComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
