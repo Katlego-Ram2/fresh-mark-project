3772c170a5f30c4aeca1c95d442cfb38a14f6f4c
Nceba Jwili <Nceba.Jwili@ekurhuleni.gov.za>
2023-08-04 14:12:58 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCommonModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatCommonModule,
+    MatDatepickerModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatPaginatorModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
